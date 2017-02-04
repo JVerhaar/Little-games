729abd144f9899d1e4ed4aa9c267debb8720fd5b
JVerhaar <0907856@hr.nl>
2017-02-04 22:52:55 +0100

    Cleaned the code

--- TotemCarver main/TotemCarver main/TotemCarver_main.py
+++ TotemCarver main/TotemCarver main/TotemCarver_main.py
@@ -9,13 +9,9 @@ class GameMenu():
     def __init__(self, screen):
         self.screen = screen
         self.background = pygame.image.load('D:/jessi/Documents/Totemcarver main/placeholder beginningscreen.png')#background start screen
-        self.clock = pygame.time.Clock()
-
+       
     def run(self):
         while True:
-            #Limit frame speed to 50FPS
-            self.clock.tick(50)
-
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
