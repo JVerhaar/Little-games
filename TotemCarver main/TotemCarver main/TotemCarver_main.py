7f3cc7259b986ab55d6f26f09ba38c7fad2ba2f6
JVerhaar <0907856@hr.nl>
2017-02-05 23:04:31 +0100

    Button works now, not going to anything yet
    Quit does fully work tho

--- TotemCarver main/TotemCarver main/TotemCarver_main.py
+++ TotemCarver main/TotemCarver main/TotemCarver_main.py
@@ -17,28 +17,22 @@ def text_objects(text, font): #For text making purposes
     textSurface = font.render(text, True, alice_blue)
     return textSurface, textSurface.get_rect()
 
-def Button(msg, x, y, w, h, ic, ac):
+def Button(msg, x, y, w, h, ic, ac, action=None):
     mouse = pygame.mouse.get_pos() #makes sure you can click on stuff
+    click = pygame.mouse.get_pressed()
     if x + w > mouse[0] > x and y + h > mouse[1] > y: #if the x coordinate + width  of the button = bigger than the 0 x position of the mouse than the x coordinate of the button(right side and same with left side)
-        pygame.draw.rect(gameDisplay, ac, (x,y,w,h))#x, y, width, length
+        pygame.draw.rect(gameDisplay, ac, (x,y,w,h))#
+        if click[0] == 1 and action != None:
+            if action == "Play":
+                game_loop()
+            elif action == "Quit":
+                pygame.quit()
+                quit()
     else:
         pygame.draw.rect(gameDisplay, ic, (x,y,w,h))
             
     #Text on the button
     smallText = pygame.font.Font("freesansbold.ttf", 30)
-    textSurf, textRect = text_objects(msg, smallText)
-    textRect.center = ((x + (w / 2)), (y + (h / 2))) #Put text in the center
-    gameDisplay.blit(textSurf, textRect)
-
-def Button2(msg, x, y, w, h, ic, ac):
-    mouse = pygame.mouse.get_pos() #makes sure you can click on stuff
-    if x + w > mouse[0] > x and y + h > mouse[1] > y: #if the x coordinate + width  of the button = bigger than the 0 x position of the mouse than the x coordinate of the button(right side and same with left side)
-        pygame.draw.rect(gameDisplay, ac, (x,y,w,h))#x, y, width, length
-    else:
-        pygame.draw.rect(gameDisplay, ic, (x,y,w,h))
-            
-    #Text on the button
-    smallText = pygame.font.Font("freesansbold.ttf", 10)
     textSurf, textRect = text_objects(msg, smallText)
     textRect.center = ((x + (w / 2)), (y + (h / 2))) #Put text in the center
     gameDisplay.blit(textSurf, textRect)
@@ -58,8 +52,8 @@ class GameMenu(): #Game menu
             #Makes the screen
             gameDisplay.blit(self.background,(0,0,))
 
-            Button("Play", 493,579,250,80, blue, light_blue)
-            Button2("Quit", 1210,650,50,50, red, bright_red)
+            Button("Play", 493,579,250,80, blue, light_blue,"Play")
+            Button("Quit", 1210,650,50,50, red, bright_red,"Quit")
             
             pygame.display.update()#updates the screen?
 
