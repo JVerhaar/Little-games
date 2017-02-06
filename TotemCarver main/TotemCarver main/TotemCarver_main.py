49f88c50fa218f1829d3b5194c7acd006959ddab
JVerhaar <0907856@hr.nl>
2017-02-06 19:35:54 +0100

    Working on fixing buttons
    Actually goes to the new screen now, but only shows up for a second

--- TotemCarver main/TotemCarver main/TotemCarver_main.py
+++ TotemCarver main/TotemCarver main/TotemCarver_main.py
@@ -15,24 +15,27 @@ light_blue = (135,206,250)
 alice_blue = (240, 248, 255)
 red = (200,0,0)
 bright_red = (255,0,0)
+background_image = pygame.image.load('D:/jessi/Documents/Totemcarver main/Placeholder background game.png')
 
 def text_objects(text, font): #For text making purposes
     textSurface = font.render(text, True, alice_blue)
     return textSurface, textSurface.get_rect()
 
-def Button(msg, x, y, w, h, ic, ac, action=None):
+def Button(msg, x, y, w, h, ic, ac, fs, action=None, action2=None):
     mouse = pygame.mouse.get_pos() #makes sure you can click on stuff
     click = pygame.mouse.get_pressed()
     if x + w > mouse[0] > x and y + h > mouse[1] > y: #if the x coordinate + width  of the button = bigger than the 0 x position of the mouse than the x coordinate of the button(right side and same with left side)
-        pygame.draw.rect(gameDisplay, ac, (x,y,w,h))#
+        pygame.draw.rect(gameDisplay, ac, (x,y,w,h))
         
+        if click[0] == 1 and action2 != None:
+            action2()
         if click[0] == 1 and action != None:
             action()
     else:
         pygame.draw.rect(gameDisplay, ic, (x,y,w,h))
             
     #Text on the button
-    smallText = pygame.font.Font("freesansbold.ttf", 30)
+    smallText = pygame.font.Font("freesansbold.ttf", fs)
     textSurf, textRect = text_objects(msg, smallText)
     textRect.center = ((x + (w / 2)), (y + (h / 2))) #Put text in the center
     gameDisplay.blit(textSurf, textRect)
@@ -56,29 +59,28 @@ class GameMenu(): #Game menu
             #Makes the screen
             gameDisplay.blit(self.background,(0,0,))
 
-            Button("Play!", 493,579,250,80, blue, light_blue,game_loop)
-            Button("Quit", 1210,650,50,50, red, bright_red,quitgame)
+            Button("Play!", 493,579,250,80, blue, light_blue, 30, new_screen, game_loop)
+            Button("Quit", 1210,650,50,50, red, bright_red, 10, quitgame, None)
             
             pygame.display.update()#updates the screen?
 
-def game_loop():
-    game_bg = pygame.image.load('D:/jessi/Documents/Totemcarver main/Placeholder background game.png')
-    gameDisplay.blit(game_bg, (0,0))
+def new_screen():
+    size = (display_width, display_height)
+    pygame.init()
+    screen = pygame.display.set_mode(size)
+    screen.blit(background_image, [0,0])
+
+    pygame.display.flip()
     pygame.display.update()
 
-def Log(x, y):
-    gameDisplay.blit('D:/jessi/Documents/Totemcarver main/Log.png')
 
-def Carved_totem(x, y):
-    gameDisplay.blit('D:/jessi/Documents/Totemcarver main/Empty totem.png')
+def game_loop():
+    game_bg = background_image
 
-def Dyed_totem(x,y):
-    gameDisplay.blit('D:/jessi/Documents/Totemcarver main/Paint red totem.png')
+    gameExit = False
 
-def Totem(x,y):
-    gameDisplay.blit('D:/jessi/Documents/Totemcarver main/Finished totem.png')
-
-
+    gameDisplay.blit(game_bg, (0,0))
+    pygame.display.update()
 
 #Runs all the things!   
 gm = GameMenu(gameDisplay)
@@ -86,3 +88,4 @@ gm.run()
 game_loop()
 pygame.quit()
 quit()
+new_screen()
