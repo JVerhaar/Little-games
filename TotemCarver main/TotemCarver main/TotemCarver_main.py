5b61a0c3133713d163e798fbedb9d8f60b609fb7
JVerhaar <0907856@hr.nl>
2017-02-07 12:31:18 +0100

    Button function changed to class

--- TotemCarver main/TotemCarver main/TotemCarver_main.py
+++ TotemCarver main/TotemCarver main/TotemCarver_main.py
@@ -21,24 +21,31 @@ def text_objects(text, font): #For text making purposes
     textSurface = font.render(text, True, alice_blue)
     return textSurface, textSurface.get_rect()
 
-def Button(msg, x, y, w, h, ic, ac, fs, action=None, action2=None):
-    mouse = pygame.mouse.get_pos() #makes sure you can click on stuff
-    click = pygame.mouse.get_pressed()
-    if x + w > mouse[0] > x and y + h > mouse[1] > y: #if the x coordinate + width  of the button = bigger than the 0 x position of the mouse than the x coordinate of the button(right side and same with left side)
-        pygame.draw.rect(gameDisplay, ac, (x,y,w,h))
-        
-        if click[0] == 1 and action2 != None:
-            action2()
-        if click[0] == 1 and action != None:
-            action()
-    else:
-        pygame.draw.rect(gameDisplay, ic, (x,y,w,h))
-            
-    #Text on the button
-    smallText = pygame.font.Font("freesansbold.ttf", fs)
-    textSurf, textRect = text_objects(msg, smallText)
-    textRect.center = ((x + (w / 2)), (y + (h / 2))) #Put text in the center
-    gameDisplay.blit(textSurf, textRect)
+class button:
+	def __init__(self, msg, x, y, w, h, fs, action=None, action2 = None):
+		self.msg = msg
+		self.X = x
+		self.Y = y
+		self.Width = w
+		self.Height = h
+		self.IColor = (50,50,50)
+		self.AColor = (100,100,100)
+		self.FontSize = fs 
+		self.Pressed = False
+	
+	def Hover(self):
+		if self.X < pygame.mouse.get_pos()[0] < self.X + self.Width:
+			if self.Y < pygame.mouse.get_pos()[1] < self.Y + self.Height:
+				return True
+		return False
+	
+	def draw(self):#methods hebben altijd self nodig
+		if not self.Hover():
+			pygame.draw.rect(gameDisplay, self.IColor, (self.X, self.Y, self.Width, self.Height))
+		else:
+			pygame.draw.rect(gameDisplay, self.AColor, (self.X, self.Y, self.Width, self.Height))
+
+		
 
 def quitgame():
     pygame.quit()
@@ -55,13 +62,11 @@ class GameMenu(): #Game menu
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     quit()
-
+            
             #Makes the screen
             gameDisplay.blit(self.background,(0,0,))
-
-            Button("Play!", 493,579,250,80, blue, light_blue, 30, new_screen, game_loop)
-            Button("Quit", 1210,650,50,50, red, bright_red, 10, quitgame, None)
-            
+            Button_Play.draw()
+           
             pygame.display.update()#updates the screen?
 
 def new_screen():
@@ -80,9 +85,11 @@ def game_loop():
     gameExit = False
 
     gameDisplay.blit(game_bg, (0,0))
+    Button_Play.draw()
     pygame.display.update()
 
-#Runs all the things!   
+#Runs all the things!  
+Button_Play = button("Play!",10,10,100,50,30) 
 gm = GameMenu(gameDisplay)
 gm.run()
 game_loop()
